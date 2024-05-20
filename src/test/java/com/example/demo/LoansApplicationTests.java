f21ee0887802824ea18679852bef6522fcca6629
Alberico Silva <alberico.rodrigo@gmail.com>
2024-05-19 23:21:32 -0300

    fix: UT

--- src/test/java/com/example/demo/LoansApplicationTests.java
+++ src/test/java/com/example/demo/LoansApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.srm.demo.LoansApplication;
+
+@SpringBootTest(classes = LoansApplication.class)
 class LoansApplicationTests {
 
 	@Test
